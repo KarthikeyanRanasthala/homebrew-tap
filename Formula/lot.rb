97b644d78feeb3571138aed8716b494019e2a21a
KarthikeyanRanasthala <karthikeyan@lottiefiles.com>
2026-07-16 11:03:44 +0530

    chore: update lot to 0.3.0

--- Formula/lot.rb
+++ Formula/lot.rb
@@ -1,14 +1,14 @@
 class Lot < Formula
   desc "Inspect Lottie and dotLottie files in the terminal"
   homepage "https://github.com/KarthikeyanRanasthala/lot"
-  url "https://github.com/KarthikeyanRanasthala/lot/releases/download/v0.2.2/lot-0.2.2-source.tar.gz"
-  sha256 "6a03b09748a797830b70ebd3ce659f3f8aa31dc4c982945744069c0e052e79aa"
+  url "https://github.com/KarthikeyanRanasthala/lot/releases/download/v0.3.0/lot-0.3.0-source.tar.gz"
+  sha256 "89c511d051ea94631e5aac4484950a466bd3e1218996914aded6d4b1bb4bed71"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/KarthikeyanRanasthala/lot/releases/download/v0.2.2"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "dae08fd467ddb082c8b0b13344a8423f27d8f2f93556834b48108226b75d2df0"
-    sha256 cellar: :any_skip_relocation, tahoe:       "2be81ff61ab540a5e06dddf94fff4ce0f7e8eeb8733cf990a5e6ad9e85bfe498"
+    root_url "https://github.com/KarthikeyanRanasthala/lot/releases/download/v0.3.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "25e10bd8400c6df638fb4c576f8cf47af6282b8247267200e9bc570e6734cf0d"
+    sha256 cellar: :any_skip_relocation, tahoe:       "d7b4463fe83ac61f6eeb89ee2b5d0d0336501fd77c7550c653b913531405fb84"
   end
 
   depends_on "rust" => :build
